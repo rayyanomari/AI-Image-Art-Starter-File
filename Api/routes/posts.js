dcf7c1a1c961d4576148bcd891fecd19d3b88a8d
RYHost <ryhost12@gmail.com>
2024-11-01 15:35:40 +0300

    .

--- Api/routes/posts.js
+++ Api/routes/posts.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const {
-  registerController,
-  loginController,
-  logoutController,
-  refetchUserController,
-} = require("../controllers/authController");
+    createPostWithImagesController_V3,
+    createPostWithImagesController_V2,
+    getPostsController,
+    getSinglePostController,
+    getUserPostsController,
+    deletePostController,
+    likePostController,
+    dislikePostController,
+} = require("../controllers/postController");
 const router = express.Router();
+
+
+router.post("/create/v3/:userId", createPostWithImagesController_V3);
+router.post("/create/v2/:userId", createPostWithImagesController_V2);
+
+router.get("/all", getPostsController);
+router.get("/single/:postId", getSinglePostController);
+router.get("/user/:userId", getUserPostsController);
+
+router.delete("/delete/:postId", deletePostController);
+router.post("/like/:postId", likePostController);
+router.post("/dislike/:postId", dislikePostController);
+
+module.exports = router;
